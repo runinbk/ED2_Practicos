bdb33d03bd03f5aa1e6f3ecb2b69e51e7436494f
runinbk <runinbk@gmail.com>
2022-12-07 06:41:47 -0400

    Practico de Arboles binarios RESUELTO (menos el ejer #9)

--- Bst/src/Arbol/Arbol.java
+++ Bst/src/Arbol/Arbol.java
@@ -133,7 +133,265 @@ public class Arbol {
 
     // PREGUNTA 1
     public int cantGajos() {
+        return cantGajos(Raiz) - 1;
+    }
 
+    private int cantGajos(Nodo T) {
+        if (T == null)
+            return 0;
+        if (hoja(T))
+            return 1;
+        int c = 0;
+        c = c + cantGajos(T.getHI());
+        c = c + cantGajos(T.getHD());
+        return c + 1;
+    }
+
+    // PREGUNTA 2
+    public void descendientes(int x) {
+        System.out.print("Descendientes de " + x + ": ");
+        descendientes(getNodo(x), x);
+
+    }
+
+    private void descendientes(Nodo T, int x) {
+        if (T == null)
+            return;
+        if (T.getData() != x)
+            System.out.print(T.getData() + ", ");
+        descendientes(T.getHI(), x);
+        descendientes(T.getHD(), x);
+
+    }
+
+    private Nodo getNodo(int x) {
+        return getNodo(Raiz, x);
+    }
+
+    private Nodo getNodo(Nodo T, int x) {
+        if (T == null)
+            return null;
+        if (T.getData() == x)
+            return T;
+        Nodo p = getNodo(T.getHI(), x);
+        if (p != null)
+            return p;
+        return getNodo(T.getHD(), x);
+    }
+
+    // PREGUNTA 3
+
+    public void podar() {
+        podar(Raiz);
+    }
+
+    private void podar(Nodo T) {
+        if (T != null) {
+            if (hoja(T.getHI())) {
+                T.setHI(null);
+            }
+            if (hoja(T.getHD())) {
+                T.setHD(null);
+            }
+            podar(T.getHI());
+            podar(T.getHD());
+
+        }
+    }
+
+    // PREGUNTA 4
+
+    public boolean isPadre(int p, int h) {
+        return isPadre(Raiz, p, h);
+    }
+
+    private boolean isPadre(Nodo T, int p, int h) {
+        if (T == null)
+            return false;
+        if (T.getData() == p) {
+            if (T.getHI() != null && T.getHI().getData() == h)
+                return true;
+            if (T.getHD() != null && T.getHD().getData() == h)
+                return true;
+        }
+        return isPadre(T.getHI(), p, h) || isPadre(T.getHD(), p, h);
+    }
+
+    // PREGUNTA 5
+
+    public boolean isHoja(int x) {
+        return isHoja(Raiz, x);
+    }
+
+    private boolean isHoja(Nodo T, int x) {
+        if (T == null)
+            return false;
+        if (T.getData() == x) {
+            if (T.getHI() == null && T.getHD() == null)
+                return true;
+        }
+        return isHoja(T.getHI(), x) || isHoja(T.getHD(), x);
+    }
+
+    // PREGUNTA 6 ===>>> SIN RECURCION
+
+    public void delHoja(int x) {
+        Nodo Ant = null;
+        Nodo p = Raiz;
+        while (p != null && p.getData() != x) {
+            Ant = p;
+            if (x < p.getData())
+                p = p.getHI();
+            else if (x > p.getData())
+                p = p.getHD();
+        }
+        if (hoja(p)) {
+            if (Ant.getHI() == p)
+                Ant.setHI(null);
+            else
+                Ant.setHD(null);
+        }
+
+    }
+
+    // private void delHoja(Nodo T, int x) {
+    // if (T != null) {
+    // if (hoja(T.getHI()) && T.getHI().getData() == x) {
+    // T.setHI(null);
+    // }
+    // if (hoja(T.getHD()) && T.getHD().getData() == x) {
+    // T.setHD(null);
+    // }
+    // delHoja(T.getHI(), x);
+    // delHoja(T.getHD(), x);
+
+    // }
+    // }
+
+    // PREGUNTA 7
+
+    public int cantLadders() {
+        return cantLadders(Raiz);
+    }
+
+    private int cantLadders(Nodo T) {
+        if (T == null)
+            return 0;
+        if (hoja(T))
+            return 0;
+        int c = 0;
+        if (isLadder(T))
+            c = 1;
+        c = c + cantLadders(T.getHI());
+        c = c + cantLadders(T.getHD());
+        return c;
+
+    }
+
+    private boolean isLadder(Nodo T) {
+        if (T != null) {
+            if (T.getHD() != null && T.getHI() != null
+                    && T.getHD().getData() == (T.getData() + 1)
+                    && T.getHI().getData() == (T.getData() - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // PREGUNTA 8
+    public void delHojaR(int x) {
+        delHojaR(Raiz, x);
+    }
+
+    private void delHojaR(Nodo T, int x) {
+        if (T != null) {
+            if (hoja(T.getHI()) && T.getHI().getData() == x) {
+                T.setHI(null);
+            }
+            if (hoja(T.getHD()) && T.getHD().getData() == x) {
+                T.setHD(null);
+            }
+            delHojaR(T.getHI(), x);
+            delHojaR(T.getHD(), x);
+
+        }
+    }
+
+    // PREGUNTA 9
+
+    // public boolean isDescIncompleto(int x) {
+    // return isDescIncompleto(Raiz, x);
+    // }
+
+    // private boolean isDescIncompleto(Nodo T, int x) {
+    // if (T == null)
+    // return false;
+    // if (T.getData() == x) {
+    // if (T.getHI() != null && T.getHD() == null)
+    // return true;
+    // if (T.getHI() == null && T.getHD() != null)
+    // return true;
+    // }
+    // return isDescIncompleto(T.getHI(), x) || isDescIncompleto(T.getHD(), x);
+    // }
+
+    // PREGUNTA 10
+
+    public boolean isBunch(int x) {
+        return isBunch(Raiz, x);
+    }
+
+    private boolean isBunch(Nodo T, int x) {
+        if (T == null)
+            return false;
+        if (T.getData() == x) { // encontrar el elemento en el nodo padre
+            if (hoja(T.getHD()) && hoja(T.getHI())) { // si los hijos son hojas
+                return true;
+            }
+        } else if (T.getHI() != null && T.getHI().getData() == x) { // si el nodo es el hijo izquierdo
+            if (hoja(T.getHD()) && hoja(T.getHI())) { // si los hijos son hojas
+                return true;
+            }
+        } else if (T.getHD() != null && T.getHD().getData() == x) { // si el nodo es el hijo derecho
+            if (hoja(T.getHD()) && hoja(T.getHI())) { // si los hijos son hojas
+                return true;
+            }
+        }
+        return isBunch(T.getHI(), x) || isBunch(T.getHD(), x);
+    }
+
+    // PREGUNTA 11
+
+    public void cutBunch(int x) {
+        cutBunch(Raiz, x);
+    }
+
+    private void cutBunch(Nodo T, int x) {
+        if (T != null) {
+            if (T.getHI() != null && T.getHI().getData() == x) { // si el nodo es el hijo izquierdo
+                if (isBunchPadre(T.getHI())) { // si los hijos son hojas
+                    T.setHD(null);
+                    T.setHI(null);
+                }
+            } else if (T.getHD() != null && T.getHD().getData() == x) { // si el nodo es el hijo derecho
+                if (isBunchPadre(T.getHD())) { // si los hijos son hojas
+                    T.setHD(null);
+                    T.setHI(null);
+                }
+            }
+            cutBunch(T.getHI(), x);
+            cutBunch(T.getHD(), x);
+        }
+    }
+
+    private boolean isBunchPadre(Nodo T) {
+        if (T != null) {
+            if (hoja(T.getHD()) && hoja(T.getHI())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- Bst/src/Arbol/Main.java
+++ Bst/src/Arbol/Main.java
@@ -2,22 +2,101 @@
 package Arbol;
 
 public class Main {
-    
-    private static final int[] datos = {3, 7, 9, 8, 3, 5};
-      
-    private static void cargarDatos(Arbol T){
+
+    private static final int[] datos = { 3, 7, 9, 8, 3, 5 };
+    // private static final int[] datos2 = { 30, 20, 60, 19, 21, 45, 70, 8, 22, 44,
+    // 46, 7, 9, 69, 71 };
+    private static final int[] datos2 = { 30, 20, 60, 19, 45, 8, 44, 46, 7, 9 };
+
+    private static void cargarDatos(Arbol T) {
         for (int i = 0; i < datos.length; i++) {
             T.add(datos[i]);
         }
     }
-    
-    
+
+    private static void cargarDatos2(Arbol T) {
+        for (int i = 0; i < datos2.length; i++) {
+            T.add(datos2[i]);
+        }
+    }
+
     public static void main(String[] args) {
         Arbol A = new Arbol();
         cargarDatos(A);
-       
+        Arbol B = new Arbol();
+        cargarDatos2(B);
+
         A.inorden();
         A.niveles();
+
+        System.out.println("Cantidad de gajos: " + A.cantGajos());
+        A.descendientes(3);
+        // A.podar();
+        // A.niveles();
+        // System.out.println();
+        // System.out.println("padre(3, 7) = " + A.isPadre(3, 7));
+        // System.out.println("padre(7, 3) = " + A.isPadre(7, 3));
+        // System.out.println("padre(7, 8) = " + A.isPadre(7, 8));
+        // System.out.println("padre(9, 8) = " + A.isPadre(9, 8));
+
+        // A.descendientes(3);
+        // System.out.println();
+        // System.out.println("isHoja(3) = " + A.isHoja(3));
+        // System.out.println("isHoja(7) = " + A.isHoja(7));
+        // System.out.println("isHoja(8) = " + A.isHoja(8));
+        // System.out.println("isHoja(9) = " + A.isHoja(9));
+        // System.out.println("isHoja(5) = " + A.isHoja(5));
+
+        // A.descendientes(3);
+        // System.out.println();
+        // System.out.println("delHoja(7)");
+        // A.delHoja(7);
+        // A.descendientes(3);
+        // System.out.println();
+        // System.out.println("delHoja(8)");
+        // A.delHoja(8);
+        // A.descendientes(3);
+        // System.out.println();
+        // System.out.println("delHoja(9)");
+        // A.delHoja(9);
+        // A.descendientes(3);
+        // System.out.println();
+        // System.out.println("delHoja(5)");
+        // A.delHoja(5);
+        // A.descendientes(3);
+        // System.out.println();
+
+        System.out.println("cantLadders() ==>> " + B.cantLadders());
+        B.inorden();
+        B.niveles();
+
+        // System.out.println("B.isDescIncompleto(7) ==>> " + B.isDescIncompleto(7));
+        // System.out.println("B.isDescIncompleto(45) ==>> " + B.isDescIncompleto(45));
+        // System.out.println("B.isDescIncompleto(19) ==>> " + B.isDescIncompleto(19));
+        // System.out.println("B.isDescIncompleto(60) ==>> " + B.isDescIncompleto(60));
+        // System.out.println("B.isDescIncompleto(20) ==>> " + B.isDescIncompleto(20));
+
+        // System.out.println("B.isBunch(19) ==>> " + B.isBunch(19));
+        // System.out.println("B.isBunch(45) ==>> " + B.isBunch(45));
+        // System.out.println("B.isBunch(9) ==>> " + B.isBunch(9));
+        // System.out.println("B.isBunch(8) ==>> " + B.isBunch(8));
+        // System.out.println("B.isBunch(46) ==>> " + B.isBunch(46));
+        // System.out.println("B.isBunch(30) ==>> " + B.isBunch(30));
+        // System.out.println("B.isBunch(1) ==>> " + B.isBunch(1));
+
+        B.cutBunch(46);
+        System.out.println("B.cutBunch(46)");
+        B.niveles();
+        B.cutBunch(8);
+        System.out.println("B.cutBunch(8)");
+        B.niveles();
+        B.cutBunch(9);
+        System.out.println("B.cutBunch(9)");
+        B.niveles();
+        B.cutBunch(45);
+        System.out.println("B.cutBunch(46)");
+        B.niveles();
+
     }
-    
+
 }
